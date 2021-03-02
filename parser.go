2485b80885fcaf7277d4685b4a26a01504004502
damnwinter <79164212+damnwinter@users.noreply.github.com>
2021-03-02 15:33:45 +0300

    Update parser.go

--- parser.go
+++ parser.go
@@ -13,6 +13,7 @@ var UserAgents = []string{
 	"Mozilla/5.0 (Windows NT 10.0; WOW64; rv:56.0) Gecko/20100101 Firefox/56.0",
 	"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36 OPR/48.0.2685.52",
 	"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.9 Safari/537.36",
+	"Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko"
 }
 
 func GetRandom(data []string) (string, error) {
